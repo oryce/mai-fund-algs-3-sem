784352d04586eed8e30fd5bf439d7b102f83430c
Sergey Mishin <serg@pochtus.ru>
2024-09-23 21:34:45 +0300

    build: add address sanitizer

--- cmake/AddTask.cmake
+++ cmake/AddTask.cmake
@@ -4,5 +4,12 @@ function(add_task task_name task_source_dir)
 
     add_executable(${task_name} ${task_src} ${lib_src})
 
+    if (APPLE AND CMAKE_BUILD_TYPE STREQUAL "ASAN")
+        add_custom_command(TARGET ${task_name} POST_BUILD
+                COMMAND dsymutil $<TARGET_FILE:${task_name}>
+                COMMENT Generate dSYM files for ${task_name}
+        )
+    endif ()
+
     target_include_directories(${task_name} PRIVATE "${CMAKE_SOURCE_DIR}/include")
 endfunction()
